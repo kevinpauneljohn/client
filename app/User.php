4cabe5a4f813a7fc5fec0617ab2015d0fb245ea1
kevinpauneljohn <johnkevinpaunel@gmail.com>
2019-12-26 18:08:38 +0800

    spatie migrations

--- app/User.php
+++ app/User.php
@@ -2,13 +2,17 @@
 
 namespace App;
 
+use App\Traits\Uuid;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes, HasRoles, LogsActivity, Uuid;
 
     /**
      * The attributes that are mass assignable.
@@ -16,7 +20,39 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'firstname',
+        'middlename',
+        'lastname',
+        'username',
+        'email',
+        'password',
+        'mobileNo',
+        'landline',
+        'birthday',
+        'address',
+        'refregion',
+        'refprovince',
+        'refcitymun',
+        'postalcode',
+        'category',
+    ];
+
+    protected static $logAttributes = [
+        'firstname',
+        'middlename',
+        'lastname',
+        'username',
+        'email',
+        'password',
+        'mobileNo',
+        'landline',
+        'birthday',
+        'address',
+        'refregion',
+        'refprovince',
+        'refcitymun',
+        'postalcode',
+        'category',
     ];
 
     /**
